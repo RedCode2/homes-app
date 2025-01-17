bbc8017f86a3d228b0438024a48843513615dd57
Rounak Karmakar <rounakk2011@gmail.com>
2025-01-17 22:34:49 +0530

    Succesful in linking src/assets to angular.json

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,11 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   imports: [],
   template: `
-    
+   <main>
+    <header class="brand-name">
+      <img class="brand-logo" src="assets/icons/logo.svg" alt="logo" aria-hidden="true">
+    </header>
+   </main>
   `,
   styleUrl: './app.component.css'
 })
