51b90f7864b1119d50ef28a26792555394c8b67a
Rounak Karmakar <rounakk2011@gmail.com>
2025-01-18 23:16:45 +0530

    Created Component 'home' and added styles to filter search bar and search button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,24 @@
 import { Component } from '@angular/core';
 
+import { HomeComponent } from './home/home.component';
+
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [HomeComponent],
   template: `
-   <main>
-    <header class="brand-name">
-      <img class="brand-logo" src="assets/icons/logo.svg" alt="logo" aria-hidden="true">
-    </header>
-    <section class="content">
-      
-    </section>
-   </main>
+    <main>
+      <header class="brand-name">
+        <img
+          class="brand-logo"
+          src="assets/icons/logo.svg"
+          alt="logo"
+          aria-hidden="true"
+        />
+      </header>
+      <section class="content">
+        <app-home></app-home>
+      </section>
+    </main>
   `,
   styleUrl: './app.component.css',
 })
