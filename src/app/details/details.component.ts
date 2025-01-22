54bb2c4f94bbd65e1f5342c21b9b2885cbd69cc2
Rounak Karmakar <rounakk2011@gmail.com>
2025-01-22 22:42:23 +0530

    Added forms to apply for house in Housing Details and added functionality to search bar in home page

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,6 +1,11 @@
 import { Component, inject } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule
+} from '@angular/forms'
 
 import { HousingLocation } from '../housing-location';
 import { HousingService } from '../housing.service';
@@ -8,7 +13,10 @@ import { HomeComponent } from '../home/home.component';
 
 @Component({
   selector: 'app-details',
-  imports: [CommonModule],
+  imports: [
+    CommonModule, 
+    ReactiveFormsModule
+  ],
   templateUrl: './details.component.html',
   styleUrl: './details.component.css'
 })
@@ -16,9 +24,23 @@ export class DetailsComponent {
   route: ActivatedRoute = inject(ActivatedRoute);
   housingService: HousingService = inject(HousingService);
   housingLocation: HousingLocation | undefined;
+  
+  applyForm: FormGroup = new FormGroup({
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+    email: new FormControl(''),
+  });
 
   constructor() {
     const housingLocationId: number = Number(this.route.snapshot.params['id']);
     this.housingLocation = this.housingService.getHousingLocationById(housingLocationId);
+  }
+
+  submitApplication() {
+    this.housingService.submitApplication(
+      this.applyForm.value.firstName ?? '',
+      this.applyForm.value.lastName ?? '',
+      this.applyForm.value.email ?? '',
+    )
   }
 }
